196b4cc1977efb797bd158564855a980dfe663b8
Paul Kang <48120607+kminh1209@users.noreply.github.com>
2022-09-15 19:33:17 +0900

    print함수 실습

---
+++ day01.py
@@ -0,0 +1,19 @@
+from color import *
+
+print(RED + "Variable" + END)
+print(GREENBG + "Paul Kang" + END)
+
+my_name = 'Paul' # C, JAVA : ' ' 한글자, " " 여러글자
+your_name = "noname" # 파이썬은 글자수 상관없이 " ", ' ' 사용가능
+print(my_name)
+print(my_name, your_name)
+print(my_name, your_name, sep='*')
+
+my_name2 = 'jw "Dangerous" lee'  # C, JAVA : "jw \"Dangerous\" lee"
+my_name3 = "jw 'Sleepy' lee"
+my_name3 = "jw 'Sleepy' lee"
+print(my_name2)
+print(my_name3)
+
+
+
